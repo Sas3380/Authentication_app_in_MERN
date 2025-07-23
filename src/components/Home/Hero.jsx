96d455eef447df31a245fc0485138a69d167e728
sayed ali shah <sayeed51003@gmail.com>
2025-07-23 20:31:37 +0500

    uploading auth app to github

---
+++ src/components/Home/Hero.jsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <section className="bg-blue-50 py-20 h-screen">
+      
+      <div className="container mx-auto my-30 text-center ">
+        <h1 className="text-6xl font-bold text-blue-800 mb-4">Welcome to AuthApp</h1>
+        <p className="text-xl text-gray-600 max-w-2xl mx-auto">
+          A secure authentication system built with MERN stack. Manage your account and enjoy our services.
+        </p>
+      </div>
+
+    </section>
+  );
+};
+
+export default Hero;
